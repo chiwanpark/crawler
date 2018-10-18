c1bdb9c91a71fea558cdc46dac3af25caf536325
Chiwan Park <chiwanpark@icloud.com>
2018-10-18 18:31:07 +0900

    Use Redis as a storage for proxies
      - Add a missing file in tests package (__init__.py)

--- crawler/proxy.py
+++ crawler/proxy.py
@@ -5,45 +5,36 @@ import re
 
 
 class ProxyProvider(object):
-    def __init__(self):
+    def __init__(self, redis):
         self._proxy_list_url = 'http://spys.me/proxy.txt'
         self._proxy_pattern = re.compile(
             '(?P<addr>[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}:[0-9]{1,5}) (?P<option>.+)')
         self._last_updated = ''
-        self._proxies = []
+        self._redis = redis
 
     def _parse_proxy_line(self, line):
         match = self._proxy_pattern.match(line)
         if not match:
             return None
 
-        addr = match.group('addr')
         option = match.group('option')
-        country = option[:2]
-        anonymity = option[3]
-        https = '-S' in option[4:]
-        google_passed = '+' == option[-1]
-
         return {
-            'addr': addr,
-            'country': country,
-            'anonymity': anonymity,
-            'support_https': https,
-            'google_passed': google_passed
+            'addr': match.group('addr'),
+            'country': option[:2],
+            'anonymity': option[3],
+            'support_https': '-S' in option[4:],
+            'google_passed': '+' == option[-1]
         }
 
-    def _parse_proxy_list(self, content):
+    async def _parse_proxy_list(self, content):
         lines = content.split('\n')
         if self._last_updated == lines[0]:
             return
         self._last_updated = lines[0]
 
-        self._proxies = []
-        for line in lines[4:]:
-            proxy = self._parse_proxy_line(line.strip())
-            if not proxy:
-                continue
-            self._proxies.append(proxy)
+        async with self._redis as redis:
+            proxies = [self._parse_proxy_line(line.strip()) for line in lines[4:]]
+            await redis.sadd(b'proxies', *proxies)
 
     async def _fetch_proxy_file(self):
         async with aiohttp.ClientSession() as session:
@@ -52,19 +43,15 @@ class ProxyProvider(object):
                     return None
                 return await res.text()
 
-    def pick(self):
-        return self._proxies[random.randrange(len(self._proxies))]
+    async def pick(self):
+        async with self._redis as redis:
+            return await self._redis.spick(b'proxies')
 
-    async def update_periodically(self):
+    async def update_once(self):
+        content = await self._fetch_proxy_file()
+        await self._parse_proxy_list(content)
+
+    async def update_periodically(self, sleep_secs=1200):
         while True:
-            content = await self._fetch_proxy_file()
-            self._parse_proxy_list(content)
-
-            await asyncio.sleep(3600)
-
-
-if __name__ == '__main__':
-    provider = ProxyProvider()
-
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(provider.update_periodically())
+            await self.update_once()
+            await asyncio.sleep(sleep_secs)
---
+++ tests/proxy_test.py
@@ -0,0 +1,28 @@
+import asynctest
+import aioredis
+import os
+from crawler.proxy import ProxyProvider
+from crawler.redis import RedisManager
+
+
+class ProxyProviderTest(asynctest.TestCase):
+    async def setUp(self):
+        self._host = os.environ.get('REDIS_HOST', 'localhost')
+        self._port = os.environ.get('REDIS_PORT', 6379)
+        self._redis = RedisManager(self._host, self._port, 'proxy-provider-test')
+
+    async def tearDown(self):
+        redis_uri = 'redis://{}:{}'.format(self._host, self._port)
+        redis = await aioredis.create_redis(redis_uri)
+        await redis.flushall()
+        redis.close()
+        await redis.wait_closed()
+
+    async def test_proxy_update(self):
+        proxies = ProxyProvider(self._redis)
+        await proxies.update_once()
+        picked = await proxies.pick()
+
+        self.assertTrue(picked is not None)
+        print(picked)
+
